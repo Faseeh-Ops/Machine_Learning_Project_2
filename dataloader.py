06c0d339acb0b9fb6ca9f121d6f959c42f8bc1f9
Faseeh-Ops <fashiali412@gmail.com>
2025-05-18 17:37:08 +0500

    some changes in it

--- dataloader.py
+++ dataloader.py
@@ -3,14 +3,14 @@ import pandas as pd
 def load_titanic_dataset():
     df = pd.read_csv("data/titanic.csv")
 
-    # Drop unnecessary columns
+
     df.drop(columns=['PassengerId', 'Name', 'Ticket', 'Cabin'], inplace=True)
 
-    # Fill missing values
+
     df['Age'] = df['Age'].fillna(df['Age'].median())
     df['Embarked'] = df['Embarked'].fillna(df['Embarked'].mode()[0])
 
-    # Encode categorical variables
+
     df = pd.get_dummies(df, columns=['Sex', 'Embarked'], drop_first=True)
 
     return df
